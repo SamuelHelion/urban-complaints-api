cfe09451946abdd72cb01ecdd87bee880a10faf8
Samuel Helion <shduartt@gmail.com>
2026-05-20 01:37:23 -0300

    atualizando recebimento de img

--- src/middlewares/uploads.ts
+++ src/middlewares/uploads.ts
@@ -1,132 +1,32 @@
-import multer, {
-    type FileFilterCallback
-} from 'multer';
-
-import path from 'path';
-import crypto from 'crypto';
-
-import { fileURLToPath } from 'url';
-
+import multer, { type FileFilterCallback } from 'multer';
 import type { Request } from 'express';
 
-const __filename =
-    fileURLToPath(
-        import.meta.url
-    );
-
-const __dirname =
-    path.dirname(
-        __filename
-    );
-
-const uploadFolder =
-    path.resolve(
-        __dirname,
-        '..',
-        'uploads'
-    );
-
-const storage =
-    multer.diskStorage({
-
-        destination(
-
-            _req: Request,
-
-            _file,
-
-            cb
-
-        ) {
-
-            cb(
-                null,
-                uploadFolder
-            );
-
-        },
-
-        filename(
-
-            _req: Request,
-
-            file,
-
-            cb
-
-        ) {
-
-            const hash =
-                crypto
-                .randomBytes(8)
-                .toString('hex');
-
-            const ext =
-                path.extname(
-                    file.originalname
-                );
-
-            cb(
-                null,
-                `${hash}-${Date.now()}${ext}`
-            );
-
-        }
-
-    });
+// 1. Alterado para usar a memória RAM (Garante o req.file.buffer)
+const storage = multer.memoryStorage();
 
 const fileFilter = (
-
     _req: Request,
-
     file: Express.Multer.File,
-
     cb: FileFilterCallback
-
 ) => {
-
     const allowedMimes = [
-
         'image/jpeg',
         'image/png',
         'image/jpg',
         'image/webp'
-
     ];
 
-    if (
-        allowedMimes.includes(
-            file.mimetype
-        )
-    ) {
-
-        return cb(
-            null,
-            true
-        );
-
+    if (allowedMimes.includes(file.mimetype)) {
+        return cb(null, true);
     }
 
-    cb(
-        new Error(
-            'Formato inválido'
-        )
-    );
-
+    cb(new Error('Formato inválido'));
 };
 
-export const upload =
-    multer({
-
-        storage,
-
-        fileFilter,
-
-        limits: {
-
-            fileSize:
-                10 * 1024 * 1024
-
-        }
-
-    });
+export const upload = multer({
+    storage,
+    fileFilter,
+    limits: {
+        fileSize: 10 * 1024 * 1024 // 10MB
+    }
+});
