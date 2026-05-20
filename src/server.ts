c80968be4f1788a9ef03e249e62150735be0a26d
Samuel Helion <shduartt@gmail.com>
2026-05-20 17:57:50 -0300

    sincronizando o cloudinary

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import "dotenv/config";
 import app from "./app.js";
 
+console.log('CLOUDINARY_CLOUD_NAME:', process.env.CLOUDINARY_CLOUD_NAME);
+console.log('CLOUDINARY_API_KEY:', process.env.CLOUDINARY_API_KEY);
+console.log('CLOUDINARY_API_SECRET:', process.env.CLOUDINARY_API_SECRET ? 'OK' : 'MISSING');
 
 app.get('/', (request, response) => {
   return response.status(200).send('Hello Backend');
