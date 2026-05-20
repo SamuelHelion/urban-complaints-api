8df43b4ab133acd859519152b0003fb6cf89daf6
Samuel Helion <shduartt@gmail.com>
2026-05-20 09:51:58 -0300

    adicionando heath

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,12 @@ app.get('/', (request, response) => {
   return response.status(200).send('Hello Backend');
 });
 
+app.get('/health', (request, response) => {
+  response.status(200).json({ status: 'ok' });
+});
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(3000, 'localhost', () => {
-  console.log("Servidor executando em http://localhost:3000");
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor executando em http://0.0.0.0:${PORT}`);
 });
