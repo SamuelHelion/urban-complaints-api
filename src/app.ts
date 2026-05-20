7f177a39ab313125c1d2a93dda5b2333f66a7a5f
Samuel Helion <shduartt@gmail.com>
2026-05-20 01:56:43 -0300

    atualizando recebimento de img

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,15 @@
-import express, { json } from "express";
+import express from 'express';
 import cors from "cors";
 import authRoutes from "./routes/auth-routes.js";
 import userRoutes from "./routes/user-routes.js";
 import postRoutes from "./routes/post-router.js";
 
+
 const app = express();
 app.use(cors());
-app.use(json({ limit: "10mb" }));
 app.use(authRoutes);
 app.use(userRoutes);
 app.use(postRoutes);
+app.use('/uploads', express.static('uploads'));
 
 export default app;
--- src/middlewares/uploads.ts
+++ src/middlewares/uploads.ts
@@ -1,32 +1,36 @@
 import multer, { type FileFilterCallback } from 'multer';
 import type { Request } from 'express';
+import path from 'path';
+import fs from 'fs';
 
-// 1. Alterado para usar a memória RAM (Garante o req.file.buffer)
-const storage = multer.memoryStorage();
+const uploadDir = 'uploads/';
+
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+    destination: (_req, _file, cb) => {
+        cb(null, uploadDir);
+    },
+    filename: (_req, file, cb) => {
+        const unique = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+        cb(null, `${unique}${path.extname(file.originalname)}`);
+    }
+});
 
 const fileFilter = (
     _req: Request,
     file: Express.Multer.File,
     cb: FileFilterCallback
 ) => {
-    const allowedMimes = [
-        'image/jpeg',
-        'image/png',
-        'image/jpg',
-        'image/webp'
-    ];
-
-    if (allowedMimes.includes(file.mimetype)) {
-        return cb(null, true);
-    }
-
+    const allowedMimes = ['image/jpeg', 'image/png', 'image/jpg', 'image/webp'];
+    if (allowedMimes.includes(file.mimetype)) return cb(null, true);
     cb(new Error('Formato inválido'));
 };
 
 export const upload = multer({
     storage,
     fileFilter,
-    limits: {
-        fileSize: 10 * 1024 * 1024 // 10MB
-    }
+    limits: { fileSize: 10 * 1024 * 1024 }
 });
